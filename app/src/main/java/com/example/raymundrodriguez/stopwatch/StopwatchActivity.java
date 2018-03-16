b4c868efa98ac8bff93223061bca3400c6dcac8d
Raymund Rodriguez <raymundrodriguez@HON-2016355.home>
2018-03-16 18:25:52 -0400

    First commit

---
+++ app/src/main/java/com/example/raymundrodriguez/stopwatch/StopwatchActivity.java
@@ -0,0 +1,88 @@
+package com.example.raymundrodriguez.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    // Sets the number of seconds on the timer
+    private void runTimer() {
+        // Get access to the text view
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        // Use a handler to post code
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                // Format the seconds into hours, minutes, and seconds
+                String time = String.format(Locale.getDefault(),
+                            "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if(running) {
+                    seconds++; // If running is true, increment the seconds
+                }
+                handler.postDelayed(this,1000); // Post the code again after 1 second
+            }
+        });
+    }
+
+}
